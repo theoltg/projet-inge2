b10c0651776181444933ed42d527e92a45709320
theoltg <theo.letang@gmail.com>
2024-05-01 13:24:29 +0200

    Correction des erreurs du fichier Test TTGO pour récuperer les valeurs du Flexsensor

--- Test_1_TTGO.ino
+++ Test_1_TTGO.ino
@@ -1,11 +1,9 @@
-#include <ESP32Servo.h>
-//#include <DHT.h>
 #include <TFT_eSPI.h>
 
+// Set up de la pin de un flexsensor
 const int flexSensorPin = 27;
 
-// Definis l'écran de la TTGO
-
+// Set up l'écran de la TTGO
 TFT_eSPI tft = TFT_eSPI();
 TFT_eSprite sprite = TFT_eSprite(&tft);
 
@@ -18,30 +16,24 @@ float charge_batterie=0.0;          // Charge de la batterie afin de l'afficher
 */
 
 //Set up de l'écran 135x240
-
 int centreX= 67 ;    
 int centreY= 120 ;
 
-//Set up du flexsensor
-int valflex;
-
-
 void setup() {
   Serial.begin(9600);
-
   tft.begin();
-  tft.fillScreen(TFT_WHITE)
-  pinMode(ledpin,OUTPUT);
-
+  tft.fillScreen(TFT_WHITE);
 }
 
 void loop() {
   int valFlex=analogRead(flexSensorPin);
   //valFlex= map(valFlex, 700,900,0,255);
+  affichage_Ecran(valFlex);
+  delay(100);
 
 }
 
-void affichage_Ecran(valFlex, charge_batterie){
+void affichage_Ecran(int valFlex){
 
   sprite.createSprite(135, 280);
   sprite.fillSprite(TFT_BLACK);
@@ -53,14 +45,6 @@ void affichage_Ecran(valFlex, charge_batterie){
   sprite.setTextColor(TFT_WHITE);
   sprite.drawString("Flex :", centerX, 40, 4);
   sprite.drawNumber(valFlex, centerX, 75, 6);
-
-
-
-
-
-
-
-
-
+  sprite.pushSprite(0,0);
 
 }
